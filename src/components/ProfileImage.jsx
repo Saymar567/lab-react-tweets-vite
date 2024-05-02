868f9a58f7989b823348823a836267e2332d69c2
Mikel Jimenez <majc1907@gmail.com>
2024-05-02 17:41:37 +0200

    done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,11 @@
+function ProfileImage(props) {
+    return (
+        <>
+<img className="profile" src={props.image}
+        alt="dog" />
+        
+        
+        </>
+    )
+}
+export default ProfileImage
